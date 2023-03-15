30e464387e9fe869d8254217586cfad0571a6383
MichaelHitchens <mmseleznev@gmail.com>
2023-03-15 11:02:51 +0300

    upd

--- lib/briefbag/configuration.rb
+++ lib/briefbag/configuration.rb
@@ -3,7 +3,6 @@
 require 'briefbag/diplomat'
 require 'anyway_config'
 require 'rainbow'
-require 'byebug'
 require 'hash_to_struct'
 
 module Briefbag
@@ -11,12 +10,12 @@ module Briefbag
     attr_reader :config, :config_name
 
     MESSAGES = {
-      notice_yml: 'NOTICE! Your app using configs from yml file',
-      notice_consul: 'NOTICE! Your app using configs from consul now
-        If you want to use local configs need to create config file. Just run `rake settings:consul2yml`',
-      error_consul: 'ALARM! You try are get consul config, but not connection to consul.
+      notice_yml: 'NOTICE! Your app is using configs from yml file',
+      notice_consul: 'NOTICE! Your app is using configs from consul now
+        If you want to use local configs. you need to create config file. Just run `rake settings:consul2yml`',
+      error_consul: 'ALARM! You are trying to get consul config, but you have no consul connection.
         Please! connect to VPN or check consul configuration',
-      error_yml: "ALARM! You try are using local schema connection! But file configuration doesn't exist!
+      error_yml: "ALARM! You are trying to use local schema connection! But file configuration doesn't exist!
         Just run `rake settings:consul2yml`"
     }.freeze
 
@@ -26,12 +25,12 @@ module Briefbag
     end
 
     def call
+      return file_config if file_exist?
+
       diplomat = Briefbag::Diplomat.new(config).call
-
-      return file_config if file_exist? && config[:environment].eql?('development')
-
-      return diplomat_config(diplomat[:consul_data]) if diplomat.success?
       return Briefbag.aborting_message(MESSAGES[:error_consul]) unless diplomat.success?
+
+      diplomat_config(diplomat[:consul_data])
     rescue StandardError
       return Briefbag.aborting_message(MESSAGES[:error_yml]) unless file_exist?
 
@@ -46,21 +45,30 @@ module Briefbag
     def file_config
       Briefbag.warning_message(MESSAGES[:notice_yml])
       data = Anyway::Config.for(:application)[config[:environment]]
-      HashToStruct.struct(data)
+
+      return HashToStruct.struct(data.deep_symbolize_keys) if defined?(Rails)
+
+      HashToStruct.struct(symbolize_all_keys(data))[config[:environment]]
     end
 
     def file_exist?
-      return true if File.exist?(yaml_file)
-
-      false
+      @file_exist ||= File.exist?(yaml_file)
     end
 
     def yaml_file
       @yaml_file ||= "./config/#{config_name}.yml"
     end
 
-    def local_keys
-      @local_keys ||= YAML.safe_load(File.read(yaml_file))[environment].keys
+
+    def symbolize_all_keys(h) # rubocop:disable  Naming/MethodParameterName
+      if h.is_a? Hash
+        h.transform_keys!(&:to_sym)
+        h.each_value do |val|
+          val.each { |v| symbolize_all_keys(v) } if val.is_a? Array
+          symbolize_all_keys(val)
+        end
+      end
+      h
     end
   end
 end
